9f1e93a5cd139044be769c9602c201c576dbc936
iahman8101 <nhuc69@gmail.com>
2020-09-03 18:34:59 +0700

    Nam

--- index.js
+++ index.js
@@ -63,3 +63,21 @@ app.get('/pages/list-your-business', (req,res)=>{
 app.get('/pages/payments-cancellation', (req,res)=>{
     res.render('paymentandcancellation');
 })
+app.get('/pages/service', (req,res)=>{
+    res.render('service');
+})
+
+const mssql = require("mssql");
+const config = {
+    server:'101.99.13.2',
+    database:'test',
+    user:'sa',
+    password:'z@GH7ytQ',
+    options: {
+        encrypt: false,
+    }
+}
+mssql.connect(config,function (err) {
+    if (err) console.log(err);
+    else console.log("connect DB thanh cong");
+});
